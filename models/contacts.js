e53018cadb5e1b46ad78e3bde943dcb4f4c1705e
z9877969 <z9877969@ukr.net>
2023-05-05 21:52:29 +0300

    initial commit

--- models/contacts.js
+++ models/contacts.js
@@ -1,29 +1,14 @@
-const fs = require("fs/promises");
+// const fs = require('fs/promises')
 
-const pathToContacts = __dirname + "./contacts.json";
+const listContacts = async () => {}
 
-const listContacts = async () => {
-  const contacts = await fs.readFile(pathToContacts);
-  return contacts;
-};
+const getContactById = async (contactId) => {}
 
-const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  return contacts.find((el) => el.id === contactId) || null;
-};
+const removeContact = async (contactId) => {}
 
-const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const contactIdx = contacts.findIndex((el) => el.id === contactId);
-  if (contactIdx !== -1) return null;
-  const removedContact = contacts.splice(contactIdx, 1);
-  await fs.writeFile(pathToContacts, contacts);
-  return removedContact;
-};
+const addContact = async (body) => {}
 
-const addContact = async (body) => {};
-
-const updateContact = async (contactId, body) => {};
+const updateContact = async (contactId, body) => {}
 
 module.exports = {
   listContacts,
@@ -31,4 +16,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-};
+}
